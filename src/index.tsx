c7a2d7ad3c91196aead8ec937139565503acb9a9
rogelio <rogelio@oneflare.com>
2021-04-15 13:37:35 +0800

    revert to css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PersonApp from './PersonaApp';
-import './index.scss';
+import './index.css';
 
 ReactDOM.render(<PersonApp />, document.getElementById('app'));
